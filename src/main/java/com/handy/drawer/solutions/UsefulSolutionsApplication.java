3e58be2d809927ff566f104a3d1bc70c2054c249
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2020-05-16 13:05:57 +0200

    first commit

---
+++ src/main/java/com/handy/drawer/solutions/UsefulSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.handy.drawer.solutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsefulSolutionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsefulSolutionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/handy/drawer/solutions/UsefulSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handy.drawer.solutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsefulSolutionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
